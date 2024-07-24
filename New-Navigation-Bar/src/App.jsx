234a3feb4a1070018b61ff42439d733435870152
Peter Gabriel Bersamina <petergabrielbersamina4@gmail.com>
2024-07-24 15:47:31 +0800

    New renditions: New-Navigation-Bar

---
+++ New-Navigation-Bar/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Gallery from './pages/Gallery'
+import About from './pages/About'
+import NoPage from './pages/NoPage'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/NoPage' element={<NoPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ New-Navigation-Bar/src/pages/NoPage.jsx
@@ -0,0 +1,8 @@
+
+export default function NoPage() {
+    return (
+        <div>
+            <h1>No Page</h1>
+        </div>
+    )
+}
