f08f9f0882f9ac12844c47cc6a48d03eb8ad7f72
Peter Gabriel Bersamina <petergabrielbersamina4@gmail.com>
2024-07-26 23:25:32 +0800

    Messy

--- New-Navigation-Bar/src/pages/About.jsx
+++ New-Navigation-Bar/src/pages/About.jsx
@@ -1,8 +1,30 @@
+import '../pages/About.css'
+import GabPhoto from '../assets/Gab.jpg'
 
 export default function About() {
     return (
         <div>
-            <h1>About</h1>
+            <main class="info">
+        <a href="BG/Gab.jpg" target="_blank">
+            <img src={GabPhoto} />
+        </a>
+
+        <a class="nameInfo" href="https://github.com/Gabewingman">
+            <h1>Peter Gabriel Bersamina</h1><br/>
+        </a>
+
+        <a class="nameSkills" href="https://www.linkedin.com/in/peter-gabriel-bersamina/">
+            <i>Developer | Web Designer | Tech Enthusiast</i>
+        </a>
+
+        <br/><br/>
+            <h2>About Me</h2><br/>
+            <i>Motivated FrontEnd Developer.</i>
+
+        <br/>
+            <p>Contact me at <strong><a class="email" href="https://mail.google.com/mail/u/0/#inbox">pgbersamina@gmail.com</a></strong></p>
+    
+    </main>
         </div>
     )
 }
